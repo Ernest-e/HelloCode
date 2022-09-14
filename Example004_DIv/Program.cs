a5aa959b1f7c1336f665efc9da96d321bea6e2a5
Ernest <rabinovichernest@gmail.com>
2022-09-14 22:51:39 +0300

    add Example005_COnditionifelse

--- Example004_DIv/Program.cs
+++ Example004_DIv/Program.cs
@@ -1,6 +1,4 @@
-﻿double numberA = new Random().Next(1, 10);
-Console.WriteLine(numberA);
-double numberB = new Random().Next(1, 10);
-Console.WriteLine(numberB);
+﻿double numberA = 12;
+double numberB = 5;
 Console.WriteLine(numberA / numberB);
 
---
+++ Example005_ConditionifElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Enter ur name: ");
+string username = Console.ReadLine ();
+
+if (username.ToLower() == "masha")
+{
+    Console.WriteLine("YEA ITS MASHA URA!");
+}
+else
+{
+    Console.Write("Hi, ");
+    Console.WriteLine(username);
+}
